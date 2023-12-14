bc802c9b4c5376ea190eb78a203141dcba2b9d12
Malte Arnold <maltearnold@hotmail.de>
2023-12-14 19:46:51 +0100

    Create initials.py

---
+++ string/initials.py
@@ -0,0 +1,11 @@
+# We want to create initials from your name and give them back in uppercase
+
+# 1. Step: create a string with your forename and surname
+forename = "Mike"
+lastname = "Smith"
+
+# 2. Step: get the first letter of your forename and surname
+initials = forename[0] + lastname[0]
+
+# 3. Step: print the initials in uppercase
+print(initials.upper())
