735445e4208962c13b1715322383bc2e9dadf7c4
Malte Arnold <maltearnold@hotmail.de>
2023-12-14 19:49:06 +0100

    Update madlibs.py

--- string/madlibs.py
+++ string/madlibs.py
@@ -7,21 +7,17 @@ age = 25
 birth_month = "August"
 
 # Option 1: 
-
 option1 = "Hello, my name is " + name + ". I am " + str(age) + " years old and I was born in " + birth_month + ". I live in " + location + "."
 print(option1)
 
 # Option 2:
-
 option2 = "Hello, my name is %s. I am %d years old and I was born in %s. I live in %s." % (name, age, birth_month, location)
 print(option2)
 
 # Option 3:
-
 option3 = "Hello, my name is {}. I am {} years old and I was born in {}. I live in {}.".format(name, age, birth_month, location)
 print(option3)
 
 # Option 4:
-
 option4 = f"Hello, my name is {name}. I am {age} years old and I was born in {birth_month}. I live in {location}."
 print(option4)
